7033e6ae0aedf46cbc70680a1a08ca8e3aeea12a
Danilo Catone <danilocatone@gmail.com>
2023-10-18 13:31:09 +0000

    update production configuration

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+if [ $# -le 0 ]
+then
+	echo "Error! Expected at least one command"
+	exit 1
+else
+	inp=$1
+fi
+
+BASE_PATH="/home/dcatone/personalsite"
+
+root_env="$BASE_PATH/.env"
+portfolio_env="$BASE_PATH/apps/portfolio/portfolio_be/.env"
+shali_env="$BASE_PATH/apps/shali/shali_be/.env"
+commando="sudo docker compose --env-file $root_env --env-file $portfolio_env --env-file $shali_env"
+
+final="$commando $inp"
+echo "Command: $final"
+printf "$($final)\n"
